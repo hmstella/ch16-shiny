6c6068125bf2e7b97e04486e319d3ee4b842f117
Hyunmin Joo <jooh2@uw.edu>
2018-05-11 17:49:59 -0700

    practice exercise 3

--- exercise-3/ui.R
+++ exercise-3/ui.R
@@ -5,21 +5,24 @@ library(shiny)
 select_values <- colnames(mpg)
 
 # Create a shinyUI with a `fluidPage` layout
-
+ui <- fluidPage(
   # A page header
-
+  h1("MPG Dataset Exploration"),
   
   # Add a select input for the x variable
-
+  selectInput("x_var", "X Variable", choices = select_values, selected = "displ"),
   
   # Add a select input for the y variable
-
+  selectInput("y_var", "Y Variable", choices = select_values, selected = "cyl"),
 
   # Add a sliderInput to set the size of each point
-
+  sliderInput("size", "Size of point", min = 1, max = 10, value = 5),
 
   # Add a selectInput that allows you to select a color from a list of choices
+  selectInput("color", "Color", choices = c("red", "blue", "yellow")),
 
-
-
+  plotOutput("scatter")
+  
   # Plot the output with the name "scatter"
+)
+shinyUI(ui)
